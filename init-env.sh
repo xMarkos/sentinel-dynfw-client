b02412089a0b9d0ad54fb3d864866ad13a5e3ea4
Marek Hübsch <xMarkos@gmail.com>
2026-08-18 22:34:48 +0200

    Improved init script to inject install dir into service unit file

--- init-env.sh
+++ init-env.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$DIR"
+
 python -m venv env --clear --system-site-packages --without-pip
 env/bin/python -m pip install -r requirements.txt
 
 [[ -e etc/variables ]] || cp etc/variables.default etc/variables
-[[ -e etc/sentinel-fw.service ]] || cp etc/sentinel-fw.service.default etc/sentinel-fw.service
+
+[[ -e etc/sentinel-fw.service ]] || {
+    sed -e "s|{{INSTALL_DIR}}|${DIR}|g" etc/sentinel-fw.service.default > etc/sentinel-fw.service
+}
